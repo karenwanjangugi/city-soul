2e3d8b72cbc5cdd8b04788b2e1f3990a40c3f8b8
karenwanjangugi <karenwanngugi@gmail.com>
2026-03-11 19:12:22 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx,css}",
-    "./src/Sections/**/*.{js,jsx,ts,tsx}",
-    "./src/Components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
